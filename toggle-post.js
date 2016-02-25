fd65c2bb2a29e0844ac57583d72b5a7358140218
John Serrano <johnserrano404@gmail.com>
2016-02-25 15:22:06 -0800

    made javascript to show/hide submission form

---
+++ toggle-post.js
@@ -0,0 +1,9 @@
+function plus_clicked() {
+	document.getElementById('submit_status_form').style.display = "flex";
+	document.getElementById('post_status').style.display = "none";
+}
+
+function arrow_clicked() {
+        document.getElementById('submit_status_form').style.display = "none";
+        document.getElementById('post_status').style.display = "";
+}
