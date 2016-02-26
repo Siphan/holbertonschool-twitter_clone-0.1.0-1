6aa907f6846ba8d96b0cda1f2eca9974ce132c4f
John Serrano <johnserrano404@gmail.com>
2016-02-25 16:20:24 -0800

    finished form according to project requirements

--- toggle-post.js
+++ toggle-post.js
@@ -1,9 +1,24 @@
 function plus_clicked() {
 	document.getElementById('submit_status_form').style.display = "flex";
 	document.getElementById('post_status').style.display = "none";
+	hideDisgusting();
 }
 
 function arrow_clicked() {
         document.getElementById('submit_status_form').style.display = "none";
         document.getElementById('post_status').style.display = "";
+}
+
+function toggle(obj) {
+	
+		if (obj.style.display != "none") {
+			obj.style.display="none"
+		} else {
+			obj.style.display="";
+		}
+}
+
+function hideDisgusting()
+{
+	toggle(document.getElementById("disgusting"));
 }
---
+++ toggle.js
@@ -0,0 +1,8 @@
+function toggle(obj) {
+
+                if (obj.style.display != "none") {
+                        obj.style.display="none"
+                } else {
+                        obj.style.display="";
+                }
+}
