dd6f362e287534cc31bdfd6687a8e08cc98fc9ba
Carole Bou <35@holbertonschool.com>
2016-04-12 20:18:45 -0700

    Fixed broken site by using backslashes before special characters

--- models/users.php
+++ models/users.php
@@ -1,8 +1,9 @@
 <?php
 $users = [
   array("id" => 1, "login" => "@JohnSerrano2", "password" => "password1", "full_name" => "John Serrano", "image" => "imgs/john.jpg", "description" => "Computers and chess. That's all there is to it."),
-  array("id" => 2, "login" => "@WebrickH", "password" => "password2", "full_name" => "Rick Houser", "image" => "imgs/rick.jpg", "description" => "Full-Stack Software Engineer at @holbertonschool. I build stuff!"),
-  array("id" => 3, "login" => "@electra_chong", "password" => "password3", "full_name" => "Electra Chong", "image" => "imgs/electra.jpg", "description" => "I like art, rpgs & josei manga! | Currently: studying fullstack dev @holbertonschool ('18) | @scrippscollege recent grad ('15")),
-  array("id" => 4, "login" => "siphan1904@gmail.com", "password" => "test", "full_name" => "Siphan Bou", "image" => "imgs/siphan.jpg", "description" => "AI/ML enthusiast. Co-founder of BobaClub Holberton. #while42. I love lame wordplay jokes."),
-];
-?>
+    array("id" => 2, "login" => "@WebrickH", "password" => "password2", "full_name" => "Rick Houser", "image" => "imgs/rick.jpg", "description" => "Full-Stack Software Engineer at @holbertonschool. I build stuff!"),
+      array("id" => 3, "login" => "@electra_chong", "password" => "password3", "full_name" => "Electra Chong", "image" => "imgs/electra.jpg", "description" => "I like art, rpgs & josei manga! | Currently: studying fullstack dev @holbertonschool \(\'18\) | @scrippscollege recent grad \(\'15\)"),
+        array("id" => 4, "login" => "siphan1904@gmail.com", "password" => "test", "full_name" => "Siphan Bou", "image" => "imgs/siphan.jpg", "description" => "AI/ML enthusiast. Co-founder of BobaClub Holberton. #while42. I love lame wordplay jokes."),
+	];
+	?>
+	
